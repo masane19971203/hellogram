12a136e27cd9a6846bb3fcde0438acfd76097c8e
masane19971203 <m03biglove-mq@docomo.ne.jp>
2020-06-12 14:47:30 +0900

    don't create file

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Hellogram
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Tokyo'
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      config.i18n.default_locale = :ja
+
   end
 end
