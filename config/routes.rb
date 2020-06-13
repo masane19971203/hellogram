2947087044a106a46100f693b09e9cd7cc936e1d
masane19971203 <m03biglove-mq@docomo.ne.jp>
2020-06-13 22:35:29 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'tweets/index'
   devise_for :users
+  root to: 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
